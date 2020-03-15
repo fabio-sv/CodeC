405a49378ed5f698e54805b9895b8ef8937e0a32
fabiosilva88 <60908416+fabiosilva88@users.noreply.github.com>
2020-03-15 00:53:41 -0300

    Add files via upload

---
+++ VetorArray.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main ()
+{
+	//Vetor Array de Indice Pré-definido 
+	 int i[5] = {1, 2, 3, 4, 5};
+	
+	printf("Valor da Posicao 0: %d \n", i[0]);
+	printf("Valor da Posicao 1: %d \n", i[1]);
+	printf("Valor da posicao 2: %d \n", i[2]);
+	printf("Valor da Posicao 3: %d \n", i[3]);
+	printf("Valor da posicao 4: %d \n", i[4]);
+	
+	system("pause");
+}
+
+
