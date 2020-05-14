a96c2f9109bd992bdbd564cec3052dfb350f6394
Fabio Silva <60908416+fabiosilva88@users.noreply.github.com>
2020-05-13 21:55:33 -0300

    Add files via upload

---
+++ Alocação Dinâmica/AlocandoMemoria.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+	
+int main (){
+	
+	int *p;
+// Sizeof retorna um ponteiro genérico...	
+	p = (int *) malloc(5 * sizeof(int)); //Aloca 20 byte
+	
+		if(p == NULL){
+			printf("Erro, memoria insuficiente!\n");
+			exit(1);
+		}
+		int i;
+		
+		for(i = 0; i < 5; i++){
+			printf("Digite o valor da posicao %d: ", i);
+			scanf("%d", &p[i]);
+		}
+	return 0;	
+}	
+		
